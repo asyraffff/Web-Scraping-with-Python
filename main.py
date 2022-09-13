7fa2a52652a88767351ad2bee2326e0163a3b200
asyraffff <amirulasyraf4.0x@gmail.com>
2022-09-13 15:29:34 +0800

    Add Beautiful Soup find & find_all() methods

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ with open('home.html', 'r') as html_file:
     # print(content)
     soup = BeautifulSoup(content, 'lxml')
     # print(soup.prettify())
+
+    # tags = soup.find('h5')
+    # tags2 = soup.find_all('h5')
+    # print(tags2)
+    # courses_html_tags = soup.find_all('h5')
+    # print(courses_html_tags)
+
+    # for course in courses_html_tags:
+    #     print(course.text)
+
+    courses_text_tags = soup.find_all('p')
+    for course_text in courses_text_tags:
+        print(course_text.text)
