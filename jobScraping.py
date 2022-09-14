59a81bba4b5a56e7f4c76fec3b842d5edfc23703
asyraffff <amirulasyraf4.0x@gmail.com>
2022-09-14 13:24:32 +0800

    Looping through the job and get the details

--- jobScraping.py
+++ jobScraping.py
@@ -8,16 +8,37 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
 
 soup = BeautifulSoup(html_text, 'lxml') # lxml : parser
 # print(soup.prettify())
-job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-# print(jobs)
-company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-published_date = job.find('span', class_='sim-posted').text
-# print(published_date)
-print(f'''
-Company name : {company_name}
-Required skills : {skills}
-Published date : {published_date}
-''')
-# for job in jobs:
+
+# for a single job
+
+# job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+# # print(jobs)
+# company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+# skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+# published_date = job.find('span', class_='sim-posted').text
+# # print(published_date)
+# print(f'''
+# Company name : {company_name}
+# Required skills : {skills}
+# Published date : {published_date}
+# ''')
+
+# for all jobs
+
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+for job in jobs:
+   company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+   skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+   published_date = job.find('span', class_='sim-posted').text
+   # print(published_date)
+
+   print(f'''
+   Company name : {company_name}
+   Required skills : {skills}
+   Published date : {published_date}
+   ''') 
+
+   print('')
+
     
